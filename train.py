bf74389d9292aa5c6bfd129d9bdd9d5c1e7f01f7
Moses Muiruri <saviamarketing.01@gmail.com>
2025-06-17 11:10:05 +0300

    from eval_iter 200 to 50

--- train.py
+++ train.py
@@ -15,9 +15,9 @@ from model import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 200
+eval_interval = 50
 log_interval = 1
-eval_iters = 200
+eval_iters = 50
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
